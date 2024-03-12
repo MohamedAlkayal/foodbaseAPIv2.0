7d1f727a9f8bd42a5ed744e1715e0837070669e6
KungFuKelo <116620255+KungFuKelo@users.noreply.github.com>
2024-03-12 23:22:07 +0200

    bcrypt => bcryptjs

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,5 @@
 require("dotenv").config;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../model/userModel");
 const CustomError = require("../utility/errorObject");
 const updateUserValidator = require("../schema/updateUserValidator");
